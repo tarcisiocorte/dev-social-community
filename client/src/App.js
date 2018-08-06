502eeb411202af68117b14e602933d95aad4728f
tarcisiocorte <tarcisiocorte@gmail.com>
2018-08-06 15:13:53 +0100

    add navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>My React App</h1>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/components/layouts/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default () => {
+  return (    
+      <footer class="bg-dark text-white mt-5 p-4 text-center">
+          Copyright &copy; {new Date().getFullYear()} Dev Connector
+      </footer>                  
+  )
+}
