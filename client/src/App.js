29d347fa5c0ca6348e1b661afbda3ced99047ff0
tarcisiocorte <tarcisiocorte@gmail.com>
2018-08-06 15:49:37 +0100

    React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
+
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-          <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Registration is working</h1>
+      </div>
+    )
+  }
+}
+export default Register;
