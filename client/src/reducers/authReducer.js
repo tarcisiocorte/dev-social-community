a38938ac8972c57cb8620412f8ab07d19ef219fe
tarcisiocorte <tarcisiocorte@gmail.com>
2018-08-09 13:05:34 +0100

    Redux Store and Chrome Extension to use redux

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    isAuthenticade: false,
+    user: {}
+}
+
+export default function(state = initialState, action){
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
